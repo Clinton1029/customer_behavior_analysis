fef964d5793f7cb8d71eef54fdf86e3f3b2dc102
clinton yade <yadeclinton20@gmail.com>
2025-03-18 11:04:26 +0300

    checked the data types of each columns

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -10,6 +10,10 @@ df = pd.read_csv(file_path)
 # Display the first 5 rows
 print(df.head())
 
+# Display data types of each column
+print("Column Data Types:\n", df.dtypes)
+
+
 # Standardize column names
 df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
 
@@ -21,5 +25,6 @@ print("Missing Values:\n", missing_values)
 # Check for duplicates
 duplicate_count = df.duplicated().sum()
 print(f"Duplicate Rows: {duplicate_count}")
+
 
 
